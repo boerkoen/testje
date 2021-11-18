7bb5e52c793582bd2e32e36abaa22fc217a1fe64
Koen D'Heedene <koene.dheedene@gmail.com>
2021-11-18 22:08:39 +0100

    Separate file for SendBroadcst.cs

---
+++ TCPServer/SendBroadcast.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remote
+{
+    public static class SendBroadcast
+    {
+        public static byte[] data = new byte[2];
+        public static Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        public static IPEndPoint iep1 = new IPEndPoint(IPAddress.Broadcast, 9050);
+
+        public static void init()
+        {
+            sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+        }
+
+        public static void sendb()
+        {
+            sock.SendTo(data, iep1);
+        }
+
+    }
+}
